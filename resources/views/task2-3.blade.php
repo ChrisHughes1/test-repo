4f4a7544353e372f986fdc6d63a49de090a67650
Chris Hughes <2266447@swansea.ac.uk>
2024-10-09 14:00:46 +0100

    lab-2-answers

---
+++ resources/views/task2-3.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Task 2.3</title>
+    </head>
+    <body>
+        <h1>Lab 2 - Task 2.3</h1>
+        <p>My first varible is: {{ $first_var }}</p>
+        <p></p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//
+//Lab 2 - Task 1
+//
+Route::get ('/hello-world', function () {
+    return 'Hello World';
+});
+
+Route::get('/capture-uri' , function () {
+    return request()->path();
+});
+
+Route::get('/return-variables', function () {
+    $first_var = 'FIRST_VAR';
+    $second_var = 'SECOND_VAR';
+    return "First Variable is: $first_var ::: Second Variable is: $second_var";
+});
+//--------------------
+
+//
+//Lab 2 - Task 2
+Route::get('/task2-1', function ()  {
+    return view('task2-1');
+});
+
+Route::get('/task2-2', function () {
+    $uri = request()->path();
+    return view('task2-2', ['uri' => $uri]);
+});
+
+Route::get('/task2-3/{first_var}', function ($first_var) {
+    return view('task2-3', ['first_var' => $first_var);
+
+});
+
+//
+
+
+
+//--------------------
 
 Route::get('/dashboard', function () {
     return view('dashboard');
